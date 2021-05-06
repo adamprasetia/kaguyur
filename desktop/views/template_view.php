e919f03ac098815195a88835837c6afb90d5081a
Adam Prasetia <adam.prasetia@kompas.com>
2021-05-06 09:20:39 +0700

    fix

--- desktop/views/template_view.php
+++ desktop/views/template_view.php
@@ -2,12 +2,13 @@
 <html class="no-js" lang="">
   <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115449429-1"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-196226270-1"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', 'UA-115449429-1');
+
+      gtag('config', 'UA-196226270-1');
     </script>
 
     <meta charset="utf-8" />
