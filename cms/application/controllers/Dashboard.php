eb906c154497872b8ce6f6c8a4987b64b08dab6d
Adam Prasetia <adam.prasetia@kompas.com>
2021-05-13 17:45:09 +0700

    fix

--- cms/application/controllers/Dashboard.php
+++ cms/application/controllers/Dashboard.php
@@ -9,7 +9,13 @@ class Dashboard extends MY_Controller {
 
 	public function index()
 	{
-		$data['content'] = $this->load->view('contents/dashboard_view', [], TRUE);
+		$this->load->model('global_model');
+		$data['content'] = $this->load->view('contents/dashboard_view', [
+			'total_product'=>$this->global_model->count(['table'=>'product']),
+			'total_member_verified'=>$this->global_model->count(['table'=>'member','where'=>['status'=>'VERIFIED']]),
+			'total_member_pending'=>$this->global_model->count(['table'=>'member','where'=>['status'=>'PENDING']]),
+			'total_member_banned'=>$this->global_model->count(['table'=>'member','where'=>['status'=>'BANNED']]),
+		], TRUE);
 
 		$this->load->view('template_view', $data);
 	}
--- cms/application/views/contents/dashboard_view.php
+++ cms/application/views/contents/dashboard_view.php
@@ -5,14 +5,69 @@
     </h1>
 </section>
 
-<!-- <div class="row">
-
+<div class="row">
     <section class="content">
-        <div class="box">
-            <div class="box-header with-border">
-                
-            </div>
-        </div>
-    </section>
+    <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3><?php echo number_format($total_product) ?></h3>
 
-</div> -->
+              <p>Produk</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="<?php echo base_url('product') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3><?php echo number_format($total_member_verified) ?></h3>
+
+              <p>Anggota Verified</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person"></i>
+            </div>
+            <a href="<?php echo base_url('member?status=VERIFIED') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3><?php echo number_format($total_member_pending) ?></h3>
+
+              <p>Anggota Pending</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person"></i>
+            </div>
+            <a href="<?php echo base_url('member?status=PENDING') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3><?php echo number_format($total_member_banned) ?></h3>
+
+              <p>Anggota Banned</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person"></i>
+            </div>
+            <a href="<?php echo base_url('member?status=BANNED') ?>" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+    </section>
+</div>
