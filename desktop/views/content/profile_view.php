09dd564f51b5d2a86965f5254c34c8af59d73610
Adam Prasetia <adam.prasetia@kompas.com>
2021-05-19 09:56:34 +0700

    fix

--- desktop/views/content/profile_view.php
+++ desktop/views/content/profile_view.php
@@ -9,7 +9,7 @@
         <span><strong><?php echo $member->farm ?></strong></span><br>
         <span><?php echo $member->address ?></span><br>
         <span><?php echo $member->phone ?></span><br>
-        <span>Status : <strong class="color-<?php echo strtolower($this->user_login['status']) ?>"><?php echo $this->user_login['status'] ?></span></strong>
+        <span>Status : <strong class="color-<?php echo strtolower($member->status) ?>"><?php echo $member->status ?></span></strong>
       </div>
     </div>
     <p><strong>Strain</strong></p>
