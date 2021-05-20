ef3996671443c6b87d74c9382056f8c6719d0eee
Adam Prasetia <adam.prasetia@kompas.com>
2021-05-20 20:43:44 +0700

    fix

--- desktop/config/routes.php
+++ desktop/config/routes.php
@@ -58,3 +58,6 @@ $route['galeri/(:num)/(:any)'] = 'galeri/detail/$1';
 $route['produk/(:num)/(:any)'] = 'produk/detail/$1';
 $route['profile/(:num)/(:any)'] = 'profile/index/$1';
 $route['forum/(:num)/(:any)'] = 'forum/detail/$1';
+
+$route['sitemap.xml'] = 'sitemap/index';
+$route['(:any)/sitemap.xml'] = 'sitemap/$1';
---
+++ desktop/controllers/Sitemap.php
@@ -0,0 +1,139 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Sitemap extends CI_Controller {
+
+	function __construct()
+   	{
+        parent::__construct();
+        $this->load->library('xml_writer');
+        $this->load->model('global_model');
+   	}
+    public function index()
+    {
+        # Initiate class
+		$xml = new Xml_writer();
+		$xml->setRootName('sitemapindex',array('xmlns'=>'http://www.sitemaps.org/schemas/sitemap/0.9'));
+        $xml->initiate();
+        $module = ['artikel','produk','anggota','forum'];
+        foreach ($module as $row) {            
+            $xml->startBranch('sitemap');
+            $xml->addNode('loc', base_url($row.'/sitemap.xml'));
+            $xml->addNode('lastmod',date('c',time()));
+            $xml->endBranch('sitemap');
+        }
+		$xml->getXml(TRUE);
+    }
+   	public function artikel()
+   	{
+		$data	= $this->global_model->get([
+            'select'=>'id, title',
+            'table'=>'article',
+            'limit'=>200,
+            'order'=>[
+                'published_date'=>'desc'
+            ],
+            'where'=>[
+                'status'=>'PUBLISH'
+            ]
+        ])->result();
+
+		# Initiate class
+		$xml = new Xml_writer();
+		$xml->setRootName('urlset', array('xmlns'=>'http://www.sitemaps.org/schemas/sitemap/0.9'));
+		$xml->initiate();
+
+		foreach ($data as $row) 
+		{
+			$xml->startBranch('url');	
+				$xml->addNode('loc', base_url('artikel/'.$row->id.'/'.url_title($row->title,'-',true)));
+			$xml->endBranch('url');	
+		}
+
+		$xml->getXml(TRUE);
+	}
+   	public function produk()
+   	{
+		$data	= $this->global_model->get([
+            'select'=>'id, name',
+            'table'=>'product',
+            'limit'=>200,
+            'order'=>[
+                'created_date'=>'desc'
+            ],
+            'where'=>[
+                'status'=>'ACTIVE'
+            ]
+        ])->result();
+
+		# Initiate class
+		$xml = new Xml_writer();
+		$xml->setRootName('urlset', array('xmlns'=>'http://www.sitemaps.org/schemas/sitemap/0.9'));
+		$xml->initiate();
+
+		foreach ($data as $row) 
+		{
+			$xml->startBranch('url');	
+				$xml->addNode('loc', base_url('produk/'.$row->id.'/'.url_title($row->name,'-',true)));
+			$xml->endBranch('url');	
+		}
+
+		$xml->getXml(TRUE);
+	}
+   	public function forum()
+   	{
+		$data	= $this->global_model->get([
+            'select'=>'id, title',
+            'table'=>'forum',
+            'limit'=>200,
+            'order'=>[
+                'created_date'=>'desc'
+            ],
+            'where'=>[
+                'status'=>1
+            ]
+        ])->result();
+
+		# Initiate class
+		$xml = new Xml_writer();
+		$xml->setRootName('urlset', array('xmlns'=>'http://www.sitemaps.org/schemas/sitemap/0.9'));
+		$xml->initiate();
+
+		foreach ($data as $row) 
+		{
+			$xml->startBranch('url');	
+				$xml->addNode('loc', base_url('forum/'.$row->id.'/'.url_title($row->title,'-',true)));
+			$xml->endBranch('url');	
+		}
+
+		$xml->getXml(TRUE);
+	}
+   	public function anggota()
+   	{
+		$data	= $this->global_model->get([
+            'select'=>'id, farm',
+            'table'=>'member',
+            'limit'=>200,
+            'order'=>[
+                'date_created'=>'desc'
+            ],
+            'where'=>[
+                'status'=>'VERIFIED'
+            ]
+        ])->result();
+
+		# Initiate class
+		$xml = new Xml_writer();
+		$xml->setRootName('urlset', array('xmlns'=>'http://www.sitemaps.org/schemas/sitemap/0.9'));
+		$xml->initiate();
+
+		foreach ($data as $row) 
+		{
+			$xml->startBranch('url');	
+				$xml->addNode('loc', base_url('profile/'.$row->id.'/'.url_title($row->farm,'-',true)));
+			$xml->endBranch('url');	
+		}
+
+		$xml->getXml(TRUE);
+	}
+}
