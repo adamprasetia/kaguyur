6c6ec0ef69e745e5dbddc726d46c6ee6a6c6aea7
Adam Prasetia <adam.prasetia@kompas.com>
2021-06-11 00:11:00 +0700

    fix

--- desktop/views/content/home_view.php
+++ desktop/views/content/home_view.php
@@ -27,26 +27,24 @@
       </div>
       <?php endif ?>    
       <div class="col-span-6 px-5 md:col-span-3">        
-        <div class="mt-5">
-          <ul>
-            <?php $i=1;foreach ($article as $row) { ?>              
-            <li class="grid grid-cols-8 gap-4 mb-5">
-              <?php if(!empty($row->image)): ?>
-              <div class="col-span-3 md:col-span-2">
-                  <img class="imgfillImg" src="<?php echo gen_thumb($row->image,'100x100') ?>" alt="<?php echo htmlentities($row->title) ?>">
-              </div>
-              <?php endif ?>
-              <div class="col-span-5 md:col-span-6">
-                  <a href="<?php echo base_url('artikel/'.$row->id.'/'.url_title($row->title,'-',true)) ?>">
-                      <h4 class="mb-2 text__wrap2"><?php echo $row->title ?></h4>
-                      <p class="text-sm text__wrap3"><?php echo $row->description ?></p>
-                      <small><?php echo format_dmy($row->published_date) ?></small>
-                  </a>
-              </div>
-            </li>
-            <?php $i++;if($i==3)break;} ?>
-          </ul>
-        </div>
+        <ul>
+          <?php $i=1;foreach ($article as $row) { ?>              
+          <li class="grid grid-cols-8 gap-4 mb-5">
+            <?php if(!empty($row->image)): ?>
+            <div class="col-span-3 md:col-span-2">
+                <img class="imgfillImg" src="<?php echo gen_thumb($row->image,'100x100') ?>" alt="<?php echo htmlentities($row->title) ?>">
+            </div>
+            <?php endif ?>
+            <div class="col-span-5 md:col-span-6">
+                <a href="<?php echo base_url('artikel/'.$row->id.'/'.url_title($row->title,'-',true)) ?>">
+                    <h4 class="mb-2 text__wrap2"><?php echo $row->title ?></h4>
+                    <p class="text-sm text__wrap3"><?php echo $row->description ?></p>
+                    <small><?php echo format_dmy($row->published_date) ?></small>
+                </a>
+            </div>
+          </li>
+          <?php $i++;if($i==3)break;} ?>
+        </ul>        
       </div>
     </div>
   </div>
