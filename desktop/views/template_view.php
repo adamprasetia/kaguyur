84057b39646d760803464aef959ee71d9d305b5e
Adam Prasetia <adam.prasetia@kompas.com>
2021-08-24 14:34:39 +0700

    fix

--- desktop/views/template_view.php
+++ desktop/views/template_view.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html class="no-js" lang="">
   <head>
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-196226270-1"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-196226270-1');
-    </script>
-
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T3JZFTZ');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8" />
     <title><?php echo isset($meta['title']) ? $meta['title'] : 'Komunitas Guppy Cianjur (KAGUYUR) Offical Website'; ?></title>
     <meta name="description" content="<?php echo isset($meta['description']) ? $meta['description'] : 'Wadah untuk membangun kekompakan dan kebersamaan serta berbagi ilmu pengetahuan seputar ikan guppy'; ?>" />
@@ -49,6 +46,10 @@
   </head>
 
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T3JZFTZ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <?php $s1 = $this->uri->segment(1); ?>
     <div class="header">
       <div class="container px-5 mx-auto">
